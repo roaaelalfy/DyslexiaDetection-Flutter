5a515f57daf64dd32dda2d24acc60afb91c9b141
Salmaelkatib <19p5976@eng.asu.edu.eg>
2024-01-24 03:15:09 +0200

    Generate random exercises for Q14-17

---
+++ lib/screens/Q14-17/GenerateRandomExercises14-17.dart
@@ -0,0 +1,183 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:dyslexiadetectorapp/core/utils/size_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
+
+class DyslexiaExerciseWidget extends StatefulWidget {
+  final int gridSize;
+  final void Function(BuildContext context) onTapFunction;
+  final void Function(BuildContext context) navigateToNextScreen;
+
+  DyslexiaExerciseWidget({
+    required this.gridSize,
+    required this.onTapFunction,
+    required this.navigateToNextScreen,});
+
+
+  @override
+  State<DyslexiaExerciseWidget> createState() => _DyslexiaExerciseWidgetState();}
+
+
+class _DyslexiaExerciseWidgetState extends State<DyslexiaExerciseWidget> {
+  Map<String, List<String>> Q14To17Map = {
+    "F": List.filled(12, 'E') + ['F'] + List.filled(12, 'E'),
+    "E": List.filled(12, 'F') + ['E'] + List.filled(12, 'F'),
+    "p": List.filled(12, 'q') + ['p'] + List.filled(12, 'q'),
+    "b": List.filled(12, 'p') + ['b'] + List.filled(12, 'p'),
+    "u": List.filled(12, 'n') + ['u'] + List.filled(12, 'n'),
+    "n": List.filled(12, 'u') + ['n'] + List.filled(12, 'u'),
+    "h": List.filled(12, 'n') + ['h'] + List.filled(12, 'n'),
+    "d": List.filled(12, 'b') + ['d'] + List.filled(12, 'b'),
+    "e": List.filled(12, 'a') + ['e'] + List.filled(12, 'a'),
+    "i": List.filled(12, 'j') + ['i'] + List.filled(12, 'j'),
+    "M": List.filled(12, 'W') + ['M'] + List.filled(12, 'W'),
+    "q": List.filled(12, 'g') + ['q'] + List.filled(12, 'g'),
+    "l": List.filled(12, 'i') + ['l'] + List.filled(12, 'i'),
+    "K": List.filled(12, 'X') + ['K'] + List.filled(12, 'X'),
+    "c": List.filled(12, 'o') + ['c'] + List.filled(12, 'o'),
+    "j": List.filled(12, 'i') + ['j'] + List.filled(12, 'i'),
+    "g": List.filled(12, 'j') + ['g'] + List.filled(12, 'j'),
+    "a": List.filled(12, 'e') + ['a'] + List.filled(12, 'e'),
+    "O": List.filled(12, 'Q') + ['O'] + List.filled(12, 'Q'),
+    "t": List.filled(12, 'f') + ['t'] + List.filled(12, 'f'),
+    "A": List.filled(12, 'V') + ['A'] + List.filled(12, 'V'),
+  };
+  late List<String> exerciseletters;
+  Random random = Random();
+
+  late FlutterTts flutterTts;
+  static bool playedSound = false;
+
+  late Timer _timer;
+  int _timerCount = 25;  // Initial timer count in seconds
+  static double progressPercentage = 1.0;
+  static bool timerStarted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initTts();
+    _initExercise();
+
+    // start timer after the sound is played to start the test
+    print("timerStarted $timerStarted");
+    if (timerStarted== false) {
+      _startTimer();
+    }
+  }
+  Future<void> _initTts() async {
+    await flutterTts.setLanguage("en-US");
+    await flutterTts.setSpeechRate(0.2);
+  }
+
+  Future<void> _initExercise() async {
+    print("playedSound: $playedSound");
+    // ensures the sound is played only once at the beginning of the exercise
+    if (!playedSound) {
+      await _initTts();
+      await loadLetterSound("Choose the different letter.");
+    }
+    exerciseletters= generateExercise(Q14To17Map);
+  }
+
+  @override
+  void dispose() {
+    flutterTts.stop(); // Stop TTS when disposing the widget
+    super.dispose();
+  }
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      print("Counter: $_timerCount percentage: $progressPercentage");
+      if (_timerCount > 0) {
+        setState(() {
+          _timerCount--;
+          progressPercentage= _timerCount/25.0;
+          timerStarted = true;
+        });
+      } else {
+        // Timer is over, navigate to the next screen
+        _timer.cancel();  // to restart timer in the new screen
+        playedSound = false;
+        timerStarted = false;
+        widget.navigateToNextScreen(context);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 220.h, vertical: 70.v),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: widget.gridSize,
+                  childAspectRatio: 1.05,
+                ),
+                itemCount: exerciseletters.length,
+                itemBuilder: (context, index) {
+                  return _buildGridTile(exerciseletters[index]);
+                },
+              ),
+            ),
+          ),
+          LinearPercentIndicator(       // Linear progress bar
+            width: MediaQuery.of(context).size.width,
+            lineHeight: 5.0,
+            percent: progressPercentage,  // Calculate the percentage based on timer count
+            backgroundColor: Colors.white,
+            progressColor: Colors.blue,
+          ),
+        ],
+      ),
+    );
+
+  }
+
+  Widget _buildGridTile(String letter) {
+    return GestureDetector(
+      child: Card(
+        elevation: 6,
+        child: Container(
+          child: Center(
+            child: Text(
+              letter,
+              style: TextStyle(fontSize: 18.0,),
+            ),
+          ),
+        ),
+      ),
+      onTap: (){
+        // save the # clicks , misses , hits then reload the screen
+        widget.onTapFunction(context);
+      },
+    );
+  }
+
+  List<String> generateExercise(Map<String, List<String>> map) {
+    // Choose a random key from the map
+    List<String> randomKey = map.keys.toList()..shuffle();
+    String selectedKey = randomKey.first;
+    exerciseletters = map[selectedKey]!;
+    exerciseletters.shuffle();
+    // remove after picked to avoid repetition
+    map.remove(selectedKey);
+
+    return exerciseletters;
+  }
+
+  Future<void> loadLetterSound(String text) async{
+    try {
+      await flutterTts.speak(text);
+      playedSound = true;
+    } catch (e) {
+      print("TTS Error: $e");
+    }
+  }
+}
